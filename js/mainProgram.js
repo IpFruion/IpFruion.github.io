23f05216ddd4eb491461e2cf9ea314f2b6d6bf8b
Derrick Lockwood <lockwood.derrick@gmail.com>
2017-10-09 11:04:22 -0500

    Saved selected tab on page reload

--- js/mainProgram.js
+++ js/mainProgram.js
@@ -7,29 +7,13 @@ var prevId = 'main';
 var tabName = "Tabs";
 var tabExpires = 5;
 
-if (detectMobile())
-{
-    print("Mobile Mode");
-    switchCss("mobile");
-}
-else
-{
-    print("Browser Mode");
-    switchCss("browser");
-}
 window.onload = function()
 {
-	// if (getCookie(tabName) === "")
-	// {
-	//     print("No Cookie Found starting One with Id: " + prevId);
-	//     setCookie(tabName, prevId, tabExpires);
-	// }
-	// setupTabs();
-
   if (window.addEventListener) {
       window.addEventListener("keydown",keyChange);
   }
 };
+
 function keyChange(event) {
 	switch(event.keyCode) {
 		case 65:
@@ -39,7 +23,7 @@ function keyChange(event) {
 			changePage('main');
 			break;
 		case 80:
-			changePage('portfolio');
+			changePage('projects');
 			break;
 		case 68:
 			changePage('documents');
@@ -49,84 +33,4 @@ function keyChange(event) {
 function print(x)
 {
     console.log(x);
-}
-function setupTabs()
-{
-    prevId = getCookie(tabName);
-
-}
-function setCookie(cname, cvalue, exdays)
-{
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
-function getCookie(cname)
-{
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++)
-    {
-        var c = ca[i].trim();
-        if (c.indexOf(name) === 0)
-            return c.substring(name.length, c.length);
-    }
-    return "";
-}
-function changePage(curId)
-{
-    prevId = getCookie(tabName);
-    if (curId === prevId)
-        return;
-    var prevTabId = prevId + 'Tab';
-    var curTabId = curId + 'Tab';
-
-    var prevTab = document.getElementById(prevTabId);
-    var curTab = document.getElementById(curTabId);
-    var prevTabContent = document.getElementById(prevId);
-    var curTabContent = document.getElementById(curId);
-    prevTabContent.style.display = 'none';
-    curTabContent.style.display = 'block';
-    setCookie(tabName, curId, tabExpires);
-    curTab.className = "selectedTab";
-    prevTab.className = prevTab.className.replace("selectedTab", "unselectedTab");
-
-    prevId = curId;
-}
-function destroyCookie()
-{
-    document.cookie = "";
-}
-function switchCss(sheet)
-{
-    var i;
-    var tag = document.getElementsByTagName("link");
-    for (i = 0; i < tag.length; i++)
-    {
-        if (tag[i].rel.indexOf("stylesheet") !== -1 && tag[i].title)
-        {
-            if (tag[i].title === sheet)
-            {
-                tag[i].disabled = false;
-            }
-            else
-            {
-                tag[i].disabled = true;
-            }
-        }
-    }
-}
-function detectMobile()
-{
-    var uagent = navigator.userAgent.toLocaleLowerCase();
-    var devices = ["iphone", "ipod", "palm", "android", "windows phone"];
-    for (var i = 0; i < devices.length; i++)
-    {
-        if (uagent.search(devices[i]) > -1)
-            return true;
-        else
-            return false;
-    }
 }
