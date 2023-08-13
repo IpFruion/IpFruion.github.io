9809043ea8ffc50d7f1e3381562de453b20dd14b
IpFruion <lockwood.derrick@gmail.com>
2023-08-13 17:21:10 -0500

    Added push from github actions

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,7 +13,10 @@ const config = {
 		adapter: adapter({
 			pages: 'docs',
 			assets: 'docs'
-		})
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
