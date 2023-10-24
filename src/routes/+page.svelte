761414a3fce456cacfa8006bba80dfd0b73a0a80
IpFruion <lockwood.derrick@gmail.com>
2023-10-23 22:48:42 -0500

    Removed section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,41 +21,38 @@
 					</h1>
 				{/if}
 			</div>
-			<section>
-				<article class="prose md:prose-lg lg:prose-xl max-w-none">
-					<div class="flex flex-col items-center md:items-start md:flex-row">
-						<img
-							class="w-48 h-48 mask mask-circle object-cover"
-							src="media/pics/profile.jpeg"
-							alt="profile"
-						/>
-						<div class="m-6">
-							<p>
-								My name is Derrick Lockwood and this is my portfolio website about the passion I
-								have for software engineering. It includes a simple biography, a timeline of my
-								career, language expertise, and some projects I have participated in throughout my
-								life.
-							</p>
-							<p>
-								I am an avid Software Developer that has many years of programming and development
-								experience. I have a Bachelors of Science in Software Engineering from Iowa State
-								University (ISU). I continue to persue my passion by working on personal projects
-								that I find interesting. I like creating software others use and can take pride in
-								the work that I develop.
-							</p>
-							<p>
-								I am currently working at a full-time position as a senior software engineer in the
-								e-commerce space. This involves creating checkout and order related backend REST
-								apis in Golang, TS, and Rust.
-							</p>
-							<p>
-								For more information see the <a class="text-primary" href="/timeline">Timeline</a>
-								page
-							</p>
-						</div>
+			<article class="prose md:prose-lg lg:prose-xl max-w-none">
+				<div class="flex flex-col items-center md:items-start md:flex-row">
+					<img
+						class="w-48 h-48 mask mask-circle object-cover"
+						src="media/pics/profile.jpeg"
+						alt="profile"
+					/>
+					<div class="m-6">
+						<p>
+							My name is Derrick Lockwood and this is my portfolio website about the passion I have
+							for software engineering. It includes a simple biography, a timeline of my career,
+							language expertise, and some projects I have participated in throughout my life.
+						</p>
+						<p>
+							I am an avid Software Developer that has many years of programming and development
+							experience. I have a Bachelors of Science in Software Engineering from Iowa State
+							University (ISU). I continue to persue my passion by working on personal projects that
+							I find interesting. I like creating software others use and can take pride in the work
+							that I develop.
+						</p>
+						<p>
+							I am currently working at a full-time position as a senior software engineer in the
+							e-commerce space. This involves creating checkout and order related backend REST apis
+							in Golang, TS, and Rust.
+						</p>
+						<p>
+							For more information see the <a class="text-primary" href="/timeline">Timeline</a>
+							page
+						</p>
 					</div>
-				</article>
-			</section>
+				</div>
+			</article>
 		</div>
 	</div>
 </div>
